99b24eeae6bd5e17d0f6be93894b4b7a87729df5
Cotel <miguelcoletomunoz@hotmail.com>
2017-12-01 11:46:29 +0100

    Moving some parts

--- cotel/src/main/scala/day1/InverseCaptcha.scala
+++ cotel/src/main/scala/day1/InverseCaptcha.scala
@@ -3,11 +3,14 @@ package day1
 object InverseCaptcha {
 
   def captcha(captcha: String): Int = {
-    val circularList = (captcha.toList :+ captcha.charAt(0)).map(_.asDigit)
+    val parsedCaptcha = captcha.toList.map(_.asDigit)
 
-    if (circularList.distinct.size == circularList.size) 0
-    else recursiveCaptcha(circularList)
+    if (!hasDuplicatedElements(parsedCaptcha)) 0
+    else recursiveCaptcha(parsedCaptcha :+ parsedCaptcha.head)
   }
+
+  private def hasDuplicatedElements(captcha: List[Int]): Boolean =
+    captcha.distinct.size != captcha.size
 
   private def recursiveCaptcha(chars: List[Int]): Int = chars match {
     case xs if xs.size <= 1 => 0
