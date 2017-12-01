5455e9e745bc7703428528f5b07062bd024add14
José Manuel Llorens <jose.llorens.ripolles@gmail.com>
2017-12-01 19:13:51 +0100

    Changed solution to oneliners because of style points

--- llorens/day01/Day01Solver.py
+++ llorens/day01/Day01Solver.py
@@ -1,31 +1,11 @@
 def solve_part1(sequence):
 
-    part1 = 0
-    size = int(len(sequence))
-
-    for i in range(size-1):
-        if sequence[i] == sequence[(i+1)]:
-            part1 += int(sequence[i])
-
-    if sequence[0] == sequence[-1]:
-        part1 += int(sequence[0])
-
-    return part1
+    return sum(int(sequence[i]) for i in range(len(sequence)) if sequence[i] == sequence[(i+1) % len(sequence)])
 
 
 def solve_part2(sequence):
 
-    part2 = 0
-    size = len(sequence)
-    displacement = int(size/2)
+    return sum(int(sequence[i]) for i in range(len(sequence)) if sequence[i] == sequence[(i+int(len(sequence)/2)) %\
+                                                                                         len(sequence)])
 
-    for i in range(displacement):
-        if sequence[i] == sequence[i+displacement]:
-            part2 += int(sequence[i])
-
-    for i in range(displacement, size):
-        if sequence[i] == sequence[(i+displacement) % size]:
-            part2 += int(sequence[i])
-
-    return part2
 
