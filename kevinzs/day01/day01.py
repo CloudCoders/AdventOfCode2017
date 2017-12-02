6e5ecde2cf872c8227dabbab9cd1e4b402795c57
Kevin <kezarszy@inf.upv.es>
2017-12-02 11:43:50 +0100

    Add tests to day 01

--- kevinzs/day01/day01.py
+++ kevinzs/day01/day01.py
@@ -3,8 +3,3 @@ def solver(input):
 
 def solver2(input):
 	return sum(int(input[x]) for x in range (len(input)) if input[x] == input[int((x+len(input)/2)%len(input))])
-
-if __name__ == "__main__":
-	input = open('input.txt').read().strip()
-	print("First Part: ", solver(input))
-	print("Second Part: ", solver2(input))
